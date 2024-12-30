433f7cd18ec22e7990a7b2af39904fd24f3b17da
niwazakura <niwazakura@outlook.com>
2024-12-30 23:22:05 +0800

    Update main.py Constant

--- main.py
+++ main.py
@@ -9,6 +9,11 @@ WIDTH = 10
 HEIGHT = 20
 LINE_CLEAR_SCORE = {1: 100, 2: 300, 3: 500, 4: 800}
 
+s_width = 800
+s_height = 700
+play_width = 300  # meaning 300 // 10 = 30 width per block
+play_height = 600  # meaning 600 // 20 = 30 height per block
+
 MOVE_LEFT = (-1, 0)
 MOVE_RIGHT = (1, 0)
 MOVE_DOWN = (0, 1)
